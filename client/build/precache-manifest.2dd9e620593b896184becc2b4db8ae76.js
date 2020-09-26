38b169ba181cd78859cc78503b1aef3f60ac0c0b
Jel <42993921+Clouddoggo@users.noreply.github.com>
2020-09-27 00:55:24 +0800

    update

---
+++ client/build/precache-manifest.2dd9e620593b896184becc2b4db8ae76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76989345f6a41e4239a05e15dba5f6bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "631566ee02822a05e4e7",
+    "url": "/static/css/2.dc17f35a.chunk.css"
+  },
+  {
+    "revision": "911760861f43a3658fbb",
+    "url": "/static/css/main.98e58ef1.chunk.css"
+  },
+  {
+    "revision": "631566ee02822a05e4e7",
+    "url": "/static/js/2.e9525d28.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e9525d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "911760861f43a3658fbb",
+    "url": "/static/js/main.49bdb486.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
